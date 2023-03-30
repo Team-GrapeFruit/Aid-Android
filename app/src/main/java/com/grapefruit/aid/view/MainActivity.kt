c3fde7851b9b51c1c866efe4963f644aa9b6df29
송현서 <s22026@gsm.hs.kr>
2023-03-30 18:16:05 +0900

    mvvm 아키텍쳐 적용

--- app/src/main/java/com/grapefruit/aid/view/MainActivity.kt
+++ app/src/main/java/com/grapefruit/aid/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.grapefruit.aid
+package com.grapefruit.aid.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.grapefruit.aid.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
