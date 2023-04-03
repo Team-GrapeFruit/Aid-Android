b6d00b4c9306594de780de1778f03a7e009c6af9
송현서 <s22026@gsm.hs.kr>
2023-04-03 21:08:02 +0900

    로그인 페이지 퍼블리싱

--- app/src/main/java/com/grapefruit/aid/view/login/LoginActivity.kt
+++ app/src/main/java/com/grapefruit/aid/view/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.grapefruit.aid.view.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.grapefruit.aid.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
